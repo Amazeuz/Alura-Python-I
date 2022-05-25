83dce26b639f529a21bd9bb7d990682d507721b9
Diuri <diuri.student@gmail.com>
2022-05-25 20:20:03 -0300

    Capítulos 2 e 3

--- novo.py
+++ novo.py
@@ -5,13 +5,21 @@ print('********************************')
 numero_secreto = 42
 
 numero_selecionado = input('Digite o seu número: ')
-
 numero_selecionado = int(numero_selecionado)
 
-if(numero_selecionado == numero_secreto):
+acertou = numero_secreto == numero_selecionado
+maior   = numero_selecionado > numero_secreto
+menor   = numero_secreto > numero_selecionado
+
+if(acertou):
     print('Você acertou !')
 else:
-    print('Você errou !')
+    if(maior):
+        print('Você errou ! O número selecionado era',
+              numero_selecionado - numero_secreto, 'números menores.')
+    if(menor):
+        print('Você errou ! O número selecionado era',
+              numero_secreto - numero_selecionado,'números maiores.')
 
 print('Seu número escolhido foi:', numero_selecionado)
 
