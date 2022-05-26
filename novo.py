d94c0c3608af0150980a7726d2285da894750528
Diuri <diuri.student@gmail.com>
2022-05-25 23:11:57 -0300

    Capítulo 4

--- novo.py
+++ novo.py
@@ -4,23 +4,31 @@ print('********************************')
 
 numero_secreto = 42
 
-numero_selecionado = input('Digite o seu número: ')
-numero_selecionado = int(numero_selecionado)
+total_de_tentativas = 3
+rodada = 1
 
-acertou = numero_secreto == numero_selecionado
-maior   = numero_selecionado > numero_secreto
-menor   = numero_secreto > numero_selecionado
+while(total_de_tentativas > 0):
 
-if(acertou):
-    print('Você acertou !')
-else:
-    if(maior):
-        print('Você errou ! O número selecionado era',
-              numero_selecionado - numero_secreto, 'números menores.')
-    if(menor):
-        print('Você errou ! O número selecionado era',
-              numero_secreto - numero_selecionado,'números maiores.')
+    print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
+    numero_selecionado = input('Digite o seu número: ')
+    numero_selecionado = int(numero_selecionado)
+    print('Seu número escolhido foi:', numero_selecionado)
 
-print('Seu número escolhido foi:', numero_selecionado)
+    acertou = numero_secreto == numero_selecionado
+    maior = numero_selecionado > numero_secreto
+    menor = numero_secreto > numero_selecionado
 
-print('Fim do jogo')
+    if(acertou):
+        print('Você acertou !')
+    else:
+        if(maior):
+            print('Você errou ! O número selecionado é menor')
+        if(menor):
+            print('Você errou ! O número selecionado é maior')
+        
+    total_de_tentativas = total_de_tentativas - 1
+    rodada = rodada + 1
+
+
+print('Fim do jogo !')
+print('O número sorteado era',numero_secreto)
