dd47e34ab85c8ef34c2ffc7ff949a9a055763318
Diuri <diuri.student@gmail.com>
2022-05-26 00:24:29 -0300

    Capítulo 5 e 6

--- novo.py
+++ novo.py
@@ -2,17 +2,23 @@ print('********************************')
 print('Bem vindo ao jogo da Adivinhação')
 print('********************************')
 
-numero_secreto = 42
+from random import randint
 
+numero_secreto = randint(1, 100)
+print(numero_secreto)
 total_de_tentativas = 3
-rodada = 1
 
-while(total_de_tentativas > 0):
+
+
+for rodada in range(1, total_de_tentativas + 1):
 
     print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
     numero_selecionado = input('Digite o seu número: ')
     numero_selecionado = int(numero_selecionado)
-    print('Seu número escolhido foi:', numero_selecionado)
+
+    if(numero_selecionado < 1 or numero_selecionado > 100):
+        print('Digite um número entre 0 e 100')
+        continue
 
     acertou = numero_secreto == numero_selecionado
     maior = numero_selecionado > numero_secreto
@@ -20,15 +26,15 @@ while(total_de_tentativas > 0):
 
     if(acertou):
         print('Você acertou !')
+        break
     else:
         if(maior):
-            print('Você errou ! O número selecionado é menor')
+            print('Você errou ! O número sorteado é menor')
         if(menor):
-            print('Você errou ! O número selecionado é maior')
-        
+            print('Você errou ! O número sorteado é maior')
+
     total_de_tentativas = total_de_tentativas - 1
-    rodada = rodada + 1
 
 
 print('Fim do jogo !')
-print('O número sorteado era',numero_secreto)
+print('O número sorteado era', numero_secreto)
