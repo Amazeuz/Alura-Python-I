43b4f179ab56e0aeedfade13fca906277f529674
Diuri <diuri.student@gmail.com>
2022-05-27 00:55:23 -0300

    Aula 2 Capítulo 1 2 e 3

--- forca.py
+++ forca.py
@@ -1,9 +1,29 @@
-# forca.py
-
 def jogar():
-
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
+
     print("Fim do jogo")
+
+
+if(__name__ == "__main__"):
+    jogar()
